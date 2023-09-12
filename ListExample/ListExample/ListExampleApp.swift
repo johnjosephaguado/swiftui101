0df4d3513887199dfcf905fa960410940d5d12a2
johnaguado <john.joseph.aguado@alaskaair.com>
2023-09-12 16:18:21 +0800

    added state and state objects exmple

--- ListExample/ListExample/ListExampleApp.swift
+++ ListExample/ListExample/ListExampleApp.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 @main
 struct ListExampleApp: App {
-    var princesses = Princess.list()
+    // Single source of truth
+    // Views @State = childView (@Binding)
+    // Data / Models / Objects @StateObject: childView (@ObservedObject)
+    
+    @StateObject var randomGenerator = RandomGenerator(list: Princess.list())
+    @StateObject var user = User()
     
     var body: some Scene {
         WindowGroup {
 //            ContentView()
-            DynamicListView(princesses: princesses)
+//            DynamicListView(princesses: princesses)
+//            RandomPrincessGeneratorStateView(princesses: princesses)
+            RandomPrincessGeneratorStateObjectView(randomGenerator: randomGenerator)
+                .environmentObject(user)
         }
     }
 }
